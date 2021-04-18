f6bc8201a9fe69f7ba3ad06cd2777c29822b1cb1
Yilin Zhao <zy2280@nyu.edu>
2021-04-19 02:38:13 +0800

    robots

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 let data;
+let w = 1000; // can change to any value
 
 function preload(){
   data = loadJSON("pose.json");
 }
 
 function setup() {
-  createCanvas(1500, 1500);
+  createCanvas(w, w);
   //console.log(data);
   // for(let i = 0; i<data.bodypoints.length; i++){
   //   let c = data.bodypoints[i];
@@ -42,6 +43,8 @@ function draw() {
   let rightShoulder = findBodypoint("rightShoulder");
   line(mapX(nose)+20*(width/1000), mapY(nose)-10*(width/1000), 0.5*(mapX(leftShoulder)+mapX(rightShoulder)), 0.5*(mapY(leftShoulder)+mapY(rightShoulder)));
 
+  let leftEar = findBodypoint("leftEar");
+  let rightEar = findBodypoint("rightEar");
   // draw points
   for(let i = 0; i<data.bodypoints.length; i++){
     let c = data.bodypoints[i];
@@ -50,19 +53,26 @@ function draw() {
     
     strokeWeight(4);
     fill(255);
+    // draw ears
     if(c.part == "leftEar" || c.part == "rightEar"){
       ellipse(xm, ym, 10*(width/1000), 20*(width/1000));
     }
+    // draw eyes
     if(c.part == "leftEye" || c.part == "rightEye"){
       fill(0);
       circle(xm, ym, 20*(width/1000));
     }
     fill(255);
+    // draw all data points
     circle(xm, ym, 10*(width/1000));
+    // draw nose && mouth
     if(c.part == "nose"){
       ellipse(xm+30*(width/2000), ym-10*(width/2000), 90*(width/1000), 100*(width/1000));
       line(xm-2.5*(width/1500), ym, xm-5*(width/1500), ym+8*(width/1500));
       line(xm+2.5*(width/1500), ym, xm+5*(width/1500), ym+8*(width/1500));
+      // mouth
+      //curve(mapX(leftEar)-20*(width/1500), mapY(leftEar)-30*(width/1500), xm-5*(width/1500), ym+30*(width/1500), xm+5*(width/1500), ym+30*(width/1500), mapX(rightEar)+50*(width/1500), mapY(rightEar)-30*(width/1500));
+      curve(mapX(leftEar)-50*(width/1500), mapY(leftEar)-60*(width/1500), xm-5*(width/1500), ym+30*(width/1500), xm+5*(width/1500), ym+30*(width/1500), mapX(rightEar)+100*(width/1500), mapY(rightEar)-60*(width/1500));
     }
     
   }
