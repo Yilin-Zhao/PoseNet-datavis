e44294af81bb06da91d0dec627a7c4b92db321f8
Yilin Zhao <zy2280@nyu.edu>
2021-04-18 15:15:31 +0800

    v1

--- sketch.js
+++ sketch.js
@@ -5,16 +5,20 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1000, 1000);
   //console.log(data);
   // for(let i = 0; i<data.bodypoints.length; i++){
   //   let c = data.bodypoints[i];
   //   print(c.part);
   // }
-  
+
 }
 
 function draw() {
   background(100);
+  for(let i = 0; i<data.bodypoints.length; i++){
+    let c = data.bodypoints[i];
+    circle(c.x, c.y, 10);
+  }
   
 }
