e11a9f69206f567740df72637d33694a99b9b518
Yilin Zhao <zy2280@nyu.edu>
2021-04-18 15:41:40 +0800

    head

--- sketch.js
+++ sketch.js
@@ -18,7 +18,13 @@ function draw() {
   background(100);
   for(let i = 0; i<data.bodypoints.length; i++){
     let c = data.bodypoints[i];
-    circle(c.x, c.y, 10);
+    xmapped = map(c.x-200, 0-200, 500-200, 0-200, 1000-200);
+    ymapped = map(c.y-60, 0-60, 500-60, 0-60, 1000-60);
+    
+    if(c.part == "nose"){
+      circle(xmapped+20, ymapped-10, 100);
+    }
+    circle(xmapped, ymapped, 10);
   }
   
 }
