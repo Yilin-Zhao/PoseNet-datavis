e7de36df09a206484d59e2f5faf5ea86d12da9a6
Yilin Zhao <zy2280@nyu.edu>
2021-04-18 15:06:55 +0800

    print json info

--- sketch.js
+++ sketch.js
@@ -6,8 +6,15 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //console.log(data);
+  // for(let i = 0; i<data.bodypoints.length; i++){
+  //   let c = data.bodypoints[i];
+  //   print(c.part);
+  // }
+  
 }
 
 function draw() {
   background(100);
+  
 }
